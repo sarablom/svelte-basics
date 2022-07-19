0856d40c85d8cdc2f500f2f66b86c8784aebebcf
Sara Blom <sara.cecilia.blom@gmail.com>
2022-07-19 19:47:05 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Max";
+  let jobTitle = "Web Developer";
+  let description = "I am a web developer";
+  let age = 30;
+
+  // let uppercaseName; not required!
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === "Maximilian") {
+    console.log("It runs!");
+    age = 31;
+  }
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "Maximilian";
+  }
+
+  function nameInput(event) {
+    const enteredValue = event.target.value;
+    name = enteredValue;
+  }
+</script>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<!-- <button on:click="{changeName}">Change Name</button> -->
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
+<input type="text" bind:value={jobTitle} />
+<input type="text" bind:value={description} />
+
+<ContactCard userName={name} {jobTitle} {description} />
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
